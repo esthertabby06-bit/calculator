8c8f4217aff8813475c4794a208794d90ee6b923
esthertabby06-bit <esthertabby06@gmail.com>
2025-11-27 21:55:01 +0300

    Updating button

---
+++ App.js
@@ -0,0 +1,5 @@
+const submitButton = document.getElementById("Submit");
+const color = document.getElementById("change color");
+submitButton.addEventListener("click", function() {
+  color.style.color = "blue";
+});
